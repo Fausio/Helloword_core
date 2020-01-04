5840862333dbf1e64e417ac27ee626e116327155
fausio luis <fausioluis@live.com>
2020-01-04 18:46:25 +0200

    Add project files.

---
+++ Helloword_core/Hello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Helloword_core
+{
+    public class Hello : IHello
+    {
+        public Hello()
+        {
+            //throw new Exception();
+        }
+  
+        string IHello.SayHello()
+        {
+            return "Hello fromn class";
+        }
+    }
+}
---
+++ Helloword_core/IHello.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helloword_core
+{
+   public interface IHello
+    {
+        string SayHello();
+    }
+}
+ 
