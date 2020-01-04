16e7781e5d9679fdf18ab557a04dc5cd86c11544
fausio luis <fausioluis@live.com>
2020-01-04 22:45:24 +0200

    adding test controler and the view

---
+++ Helloword_core/Test/Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helloword_core.Test
+{
+    //[Controller]
+    public class Test: Controller
+    {
+
+        public string Index()
+        {
+            return "Hello from HomeController TEST.CS";
+        }
+
+        public string About()
+        {
+            return "About";
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
